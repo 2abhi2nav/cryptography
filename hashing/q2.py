cb995f3d8fa05277461ff5257b69fbd9517f1d67
Abhinav <abhinavrenji@gmail.com>
2026-03-18 13:38:14 +0530

    Update italic settings

--- hashing/q2.py
+++ hashing/q2.py
@@ -13,27 +13,27 @@ def right_rotate_64(n, b):
 def md5_manual(message):
     if isinstance(message, str):
         message = message.encode('utf-8')
-    
+
     # Constants and S-box for MD5
     s = [7, 12, 17, 22] * 4 + [5, 9, 14, 20] * 4 + [4, 11, 16, 23] * 4 + [6, 10, 15, 21] * 4
     k = [int(abs(math.sin(i + 1)) * 2**32) & 0xFFFFFFFF for i in range(64)]
-    
+
     # Initial state
     a0, b0, c0, d0 = 0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476
-    
+
     # Padding
     msg_len_bits = (len(message) * 8) & 0xffffffffffffffff
     message += b'\x80'
     while (len(message) * 8) % 512 != 448:
         message += b'\x00'
     message += msg_len_bits.to_bytes(8, byteorder='little')
-    
+
     # Process blocks
     for i in range(0, len(message), 64):
         chunk = message[i:i+64]
         m = [int.from_bytes(chunk[j:j+4], byteorder='little') for j in range(0, 64, 4)]
         a, b, c, d = a0, b0, c0, d0
-        
+
         for j in range(64):
             if 0 <= j <= 15:
                 f = (b & c) | ((~b) & d)
@@ -47,15 +47,15 @@ def md5_manual(message):
             else:
                 f = c ^ (b | (~d))
                 g = (7 * j) % 16
-            
+
             temp = (a + f + k[j] + m[g]) & 0xFFFFFFFF
             a, b, c, d = d, (b + left_rotate(temp, s[j])) & 0xFFFFFFFF, b, c
-            
+
         a0 = (a0 + a) & 0xFFFFFFFF
         b0 = (b0 + b) & 0xFFFFFFFF
         c0 = (c0 + c) & 0xFFFFFFFF
         d0 = (d0 + d) & 0xFFFFFFFF
-        
+
     return b''.join(x.to_bytes(4, byteorder='little') for x in [a0, b0, c0, d0])
 
 # --- SHA-512 Implementation (Simplified Concept) ---
@@ -64,29 +64,29 @@ def sha512_manual(message):
     # for timing purposes while following algorithm logic.
     if isinstance(message, str):
         message = message.encode('utf-8')
-    
+
     h = [0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
          0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179]
-    
+
     msg_len_bits = len(message) * 8
     message += b'\x80'
     while (len(message) * 8) % 1024 != 896:
         message += b'\x00'
     message += msg_len_bits.to_bytes(16, byteorder='big')
-    
+
     for i in range(0, len(message), 128):
         for _ in range(80): 
             h[0] = (h[0] + 1) & 0xFFFFFFFFFFFFFFFF
-            
+
     return b''.join(x.to_bytes(8, byteorder='big') for x in h)
 
 def generate_mac(key, message, hash_func, block_size):
     if len(key) > block_size: key = hash_func(key)
     if len(key) < block_size: key = key.ljust(block_size, b'\x00')
-    
+
     ipad = bytes([x ^ 0x36 for x in key])
     opad = bytes([x ^ 0x5c for x in key])
-    
+
     inner_hash = hash_func(ipad + message.encode())
     return hash_func(opad + inner_hash).hex()
 
